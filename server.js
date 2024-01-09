95869d3b185b9bacb77898d293e4f0fdd0e1bc59
SARIAH TANNER <sariahtanner@gmail.com>
2024-01-08 17:29:56 -0700

    lesson1

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const lesson1Controller = require('./controllers/lesson1');
  
-app.get('/', (req, res) => {
-  res.send("Hello");
-});
+app.get('/', lesson1Controller.sariahRoute);
+app.get('/', lesson1Controller.garrikRoute);
  
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
